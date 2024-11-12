f845e5fd0f9e4215899f0604de1308e87f7a0d35
gkdl4239 <77243795+gkdl4239@users.noreply.github.com>
2024-11-12 10:05:06 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,19 +1,13 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        answer += n+1;
-        
-        if(n<2){
-            answer=n;
-        }else if(n==2){
-            answer=3;
-        }else{
-            for(int i=2; i<=n/2; i++){
+        for(int i=1; i<=n/2; i++){
                 if(n%i==0){
                     answer +=i;
             }
+        
         }
-        }
+        answer += n;
         return answer;
     }
 }
