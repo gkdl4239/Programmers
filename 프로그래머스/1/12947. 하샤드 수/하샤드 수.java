581e5d7a4c91c11213968dd01eeb5e2f7779ca29
gkdl4239 <77243795+gkdl4239@users.noreply.github.com>
2024-11-17 16:59:54 +0900

    [level 1] Title: 하샤드 수, Time: 1.73 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -2,22 +2,7 @@ import java.util.List;
 import java.util.ArrayList;
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-        int ori = x;
-        List<Integer> nums = new ArrayList<>();
-        while(x>0){
-            nums.add(x%10);
-            x= x/10;
-        }
-        
-        int sum=0;
-        for(int num : nums){
-            sum+=num;
-        }
-        
-        if(ori%sum!=0){
-            answer = false;
-        }
-        return answer;
+        int sum = String.valueOf(x).chars().map(a -> a-'0').sum();
+        return x%sum==0;
     }
 }
