36421144dadfbb1df3778c933dca4020c797cda9
gkdl4239 <77243795+gkdl4239@users.noreply.github.com>
2024-11-18 11:53:42 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 9.01 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        for(int i =0; i+p.length()<=t.length(); i++){
+            if(Long.parseLong(p)>=Long.parseLong(t.substring(i,i+p.length()))) answer++;
+            
+        }
+        return answer;
+    }
+}
