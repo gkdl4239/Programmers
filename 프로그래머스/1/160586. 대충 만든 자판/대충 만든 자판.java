01305273b9c1b82bc390c8c1996ca512565e662a
gkdl4239 <77243795+gkdl4239@users.noreply.github.com>
2024-11-27 20:37:25 +0900

    [level 1] Title: 대충 만든 자판, Time: 4.31 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,29 +1,39 @@
+import java.util.HashMap;
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
         
+        HashMap<Character,Integer> map = new HashMap<>();
         
-        for(int i=0; i<targets.length; i++){
-            
-            int count = 0;
-            
-            for(int k=0; k<targets[i].length(); k++){
-                String std = ""+targets[i].charAt(k);
-                int min = 101;
-                for(int j=0; j<keymap.length; j++){
-                    int index = keymap[j].indexOf(std);
-                    if(index!=-1){
-                        min = Math.min(min,index);
+        for(String key : keymap){
+            for(int i=0; i<key.length(); i++){
+                char ch = key.charAt(i);
+                
+                if(map.containsKey(ch)){
+                    if(i+1<map.get(ch)){
+                        map.replace(ch,i+1);
                     }
                 }
-                if(min == 101){
-                    count = -1;
+                
+                else{
+                    map.put(ch,i+1);
+                }
+            }
+        }
+        
+        for(int k=0; k<targets.length; k++){
+            
+            int count = 0;
+            for(int j=0; j<targets[k].length(); j++){
+                if(map.containsKey(targets[k].charAt(j))){
+                    count+=map.get(targets[k].charAt(j));
+                }
+                else{
+                    count=-1;
                     break;
                 }
-                count += min;
-                count++;
             }
-            answer[i]=count;
+            answer[k] = count;
         }
         
         return answer;
