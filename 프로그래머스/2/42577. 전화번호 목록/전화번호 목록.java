55ee9ff95e9e2a8f172ee6e3404f052794485b17
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-03-26 10:06:56 +0900

    [level 2] Title: 전화번호 목록, Time: 338.52 ms, Memory: 99.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        
+        Arrays.sort(phone_book);
+        
+        boolean answer = true;
+        
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            if(phone_book[i+1].indexOf(phone_book[i]) == 0) {
+                answer = false;
+            }
+        }
+        
+        return answer;
+    }
+}
