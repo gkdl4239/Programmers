3a060698bcffbff106e40f3170e9f6ea277e76c2
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-04-11 13:23:26 +0900

    [level 1] Title: K번째수, Time: 0.38 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,22 +1,20 @@
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
+import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        List<Integer> cut = new ArrayList<>();
-        List<Integer> nums = new ArrayList<>();
+        int[] answer = new int[commands.length];
         
-        for(int i=0; i<commands.length; i++){
-            for(int j=commands[i][0]-1; j<commands[i][1]; j++){
-                cut.add(array[j]);
+        for(int i = 0; i < commands.length; i++) {
+            
+            int first = commands[i][0]-1;
+            int last = commands[i][1];
+            int[] slice = new int[last - first];
+            for(int j = 0; j < last - first; j++) {
+                slice[j] = array[j+first];
             }
-            Collections.sort(cut);
-            nums.add(cut.get(commands[i][2]-1));
-            cut = new ArrayList<>();
+            Arrays.sort(slice);
+            answer[i] = slice[commands[i][2]-1];
         }
         
-        int[] answer = nums.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
