aa68238c474493b37988834c78fc784ed63e71b3
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-05-13 19:45:07 +0900

    [level 2] Title: 조이스틱, Time: 0.05 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        
+        int length = name.length();
+        
+        int[] alpha =  new int[name.length()];
+        
+        for(int i = 0; i < length; i++) {
+            char cur = name.charAt(i);
+            if(cur == 'A')
+                alpha[i] = 1;
+            
+            answer += Math.min(cur - 'A', 'Z' - cur + 1);
+        }
+        
+        int move = length - 1;
+        
+        for(int j = 0; j < length; j++) {
+            
+            int next = j+1;
+            
+            while(next < length && name.charAt(next) == 'A') {
+                next++;
+            }
+            
+            int moveRAndL = j * 2 + (length - next);
+            int moveLAndR = (length - next) * 2 + j;
+            
+            move = Math.min(move, Math.min(moveRAndL, moveLAndR));
+        }
+        answer += move;
+        return answer;
+    }
+}
