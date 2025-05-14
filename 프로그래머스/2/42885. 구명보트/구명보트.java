34f7070575d8a3667fb3cefb0aac108a97bc65e3
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-05-14 10:59:36 +0900

    [level 2] Title: 구명보트, Time: 11.58 ms, Memory: 58 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        
+        Arrays.sort(people);
+        
+        int max = people.length-1;
+        int min = 0;
+        int answer = 0;
+        
+        while(max >= min) {
+            if(people[max] + people[min] <= limit) {
+                min++;
+            }
+            max--;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
