5958a4a685c4b45c682a673503cc7381d022bcb2
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-08-25 15:01:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 98.08 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,25 +4,17 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         
         Map<String, Integer> hash = new HashMap<>();
-        
-        for(int i = 0; i < completion.length; i++) {
-            hash.put(participant[i], hash.getOrDefault(participant[i], 0) + 1);
-            hash.put(completion[i], hash.getOrDefault(completion[i], 0) - 1);
-        }
-        hash.put(participant[participant.length-1], hash.getOrDefault(participant[participant.length-1], 0) + 1);
-        
-        Set<String> keySet = hash.keySet();
-        
         String answer = "";
         
-        for(String key : keySet) {
+        for(String player : participant) hash.put(player, hash.getOrDefault(player, 0) + 1);
+        for(String player : completion) hash.put(player, hash.getOrDefault(player, 0) - 1);
+        
+        for(String key : hash.keySet()) {
             if(hash.get(key) == 1) {
                 answer = key;
                 break;
-            }    
+            }
         }
-        
         return answer;
-        
     }
 }
