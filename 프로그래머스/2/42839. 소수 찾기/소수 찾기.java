52fb9dbd9076747e2be8ed646beebe61f956da9d
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-09-01 18:08:44 +0900

    [level 2] Title: 소수 찾기, Time: 23.50 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,22 +3,38 @@ class Solution {
     
     HashSet<Integer> hash = new HashSet<>();
     
-    private void dfs(StringBuilder sb, StringBuilder numbers) {
+    
+    private void dfs(StringBuilder cases, StringBuilder numbers) {
         
-        if(!sb.toString().equals("")) {
-            hash.add(Integer.parseInt(sb.toString()));
+        if(!cases.toString().equals("")) {
+            hash.add(Integer.parseInt(cases.toString()));
         }
         
-        for(int i = 0 ; i < numbers.length(); i++) {
+        for(int i=0; i<numbers.length(); i++) {
             
             StringBuilder sliced = new StringBuilder();
             
             sliced.append(numbers.substring(0,i));
             sliced.append(numbers.substring(i+1));
             
-            dfs(new StringBuilder(sb).append(numbers.charAt(i)), sliced);
+            dfs(new StringBuilder(cases).append(numbers.charAt(i)), sliced);
         }
     }
+    
+    private boolean isPrime(int number) {
+        
+        if(number < 2) return false;
+        if(number == 2) return true;
+        if (number % 2 == 0) return false;
+        
+        for(int i=3; i<=Math.sqrt(number); i+=2) {
+            if(number%i==0) return false;    
+        }
+        
+        return true;
+    }
+    
+    
     
     public int solution(String numbers) {
         
@@ -26,18 +42,8 @@ class Solution {
         
         dfs(new StringBuilder(), new StringBuilder(numbers));
         
-        for(int number : hash) {
-            
-            if(number < 2) continue;
-            
-            boolean isPrime = true;
-            for(int i = 2; i < (int)Math.sqrt(number) + 1; i++) {
-                if(number % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if(isPrime) answer++;
+        for(int num : hash) {
+            if(isPrime(num)) answer++;
         }
         
         return answer;
