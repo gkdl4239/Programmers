0177f7b4d5012e67a33a10ba79633c9cc87f8d34
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-09-01 14:59:49 +0900

    [level 2] Title: H-Index, Time: 0.94 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,16 +1,18 @@
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
+        
         Arrays.sort(citations);
+        int answer = 0;
         
         for(int i=0; i<citations.length; i++) {
             if(citations[i] >= citations.length-i) {
-                return citations.length-i;
+                answer = citations.length-i;
+                break;
             }
         }
         
-        return 0;
+        return answer;
         
     }
 }
