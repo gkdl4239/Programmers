11cfb3f45c073d8a1a5503787a1547524d56b23c
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-09-01 16:04:23 +0900

    [level 1] Title: 모의고사, Time: 1.01 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,54 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
+import java.util.*;
 class Solution {
-    public int[] solution(int[] answers) {
-        int[] answer = {0,0,0};
+    public List<Integer> solution(int[] answers) {
         
-        int[] one = {1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] p1 = {1,2,3,4,5};
+        int[] p2 = {2,1,2,3,2,4,2,5};
+        int[] p3 = {3,3,1,1,2,2,4,4,5,5};
         
-        int i =0;
+        int[] scores = new int[3];
         
-        for(int ans : answers){
-            if(i==5) i=0;
-            if(ans==one[i]) answer[0]+=1;
-            i++;
-        }
-        i=0;
+        List<Integer> winners = new ArrayList<>();
         
-        for(int ans : answers){
-            if(i==8) i=0;
-            if(ans==two[i]) answer[1]+=1;
-            i++;
-        }
-        i=0;
-        
-        for(int ans : answers){
-            if(i==10) i=0;
-            if(ans==three[i]) answer[2]+=1;
-            i++;
+        for(int i=0; i<answers.length; i++) {
+            if(p1[i%5] == answers[i]) scores[0]++;
+            if(p2[i%8] == answers[i]) scores[1]++;
+            if(p3[i%10] == answers[i]) scores[2]++;
         }
         
-        int max = Arrays.stream(answer)
-                        .max()
-                        .orElseThrow();
+        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
         
-        List<Integer> order = new ArrayList<>();
-        
-        for(int j=0; j<3; j++){
-            if(answer[j]==max){
-                order.add(j);
-            }
+        for(int j=0; j<scores.length; j++) {
+            if(scores[j] == max) winners.add(j+1);
         }
         
-        int[] array = new int[order.size()];
-        for(int k=0; k<order.size(); k++){
-            array[k] = order.get(k)+1;
-        }
-        
-        
-        return array;
+        return winners;
     }
 }
