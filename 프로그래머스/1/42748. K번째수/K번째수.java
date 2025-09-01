0b2b73b8f463e390c6f7a8f2bd51c64b0fbc1bc5
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-09-01 14:11:00 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,18 +1,18 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
         
-        for(int i = 0; i < commands.length; i++) {
-            
-            int first = commands[i][0]-1;
-            int last = commands[i][1];
-            int[] slice = new int[last - first];
-            for(int j = 0; j < last - first; j++) {
-                slice[j] = array[j+first];
+        int[] answer = new int[commands.length];
+        List<Integer> order = new ArrayList<>();
+        
+        for(int i=0; i<commands.length; i++) {
+            for(int j = commands[i][0]-1; j<commands[i][1]; j++) {
+                order.add(array[j]);
             }
-            Arrays.sort(slice);
-            answer[i] = slice[commands[i][2]-1];
+            
+            Collections.sort(order);
+            answer[i] = order.get(commands[i][2]-1);
+            order.clear();
         }
         
         return answer;
