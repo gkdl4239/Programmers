794b223f2750613ac25136e997627cec5acf66d9
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-09-01 13:59:48 +0900

    [level 2] Title: 더 맵게, Time: 1417.61 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,21 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
         int answer = 0;
         
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        
-        for(int index : scoville) {
-            heap.add(index);
+        for(int num : scoville) {
+            heap.add(num);
         }
         
-        while(heap.peek() < K && heap.size() > 1) {
-            int mixed = heap.poll() + heap.poll()*2;
+        while(heap.peek() < K) {
+            
+            if(heap.size() < 2) return -1;
+            int mixed;
+            mixed = heap.poll() + heap.poll()*2;
             heap.add(mixed);
             answer++;
         }
-        
-        if(heap.peek() < K && heap.size() == 1) answer = -1;
         
         return answer;
     }
