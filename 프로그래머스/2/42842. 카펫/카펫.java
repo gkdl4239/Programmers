3eb369cdf2ab78a327a8ab756401b081d45ef357
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-09-02 23:10:09 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,17 @@
 
 class Solution {
     public int[] solution(int brown, int yellow) {
+        
         int[] answer = new int[2];
         
-        int sum = (brown-4)/2;
-        for(int i=1; i<sum; i++) {
-            if(i*(sum-i) == yellow) {
-                answer[0] = Math.max(i,sum-i)+2;
-                answer[1] = Math.min(i,sum-i)+2;
+        for(int i=1; i<=Math.sqrt(yellow); i++) {
+            if(yellow % i == 0 && brown == 2*i + 2*(yellow/i) + 4) {
+                answer[0] = yellow / i + 2;
+                answer[1] = i + 2;
                 break;
+                }
             }
-        }
         
         return answer;
+        }
     }
-}
