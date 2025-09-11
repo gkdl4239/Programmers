f3602ca62cc035cf664e6e8321e77c688553e4e6
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-09-11 21:49:34 +0900

    [level 2] Title: 올바른 괄호, Time: 23.86 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,14 +3,17 @@ class Solution {
     boolean solution(String s) {
         
         Stack<Character> stack = new Stack<>();
+        boolean answer = true;
         
         for(int i=0; i<s.length(); i++) {
+            
+            
             if(!stack.isEmpty() && stack.peek() == '(' && s.charAt(i) == ')') {
                 stack.pop();
+                continue;
             }
-            else{
-                stack.push(s.charAt(i));
-            }
+            
+            stack.push(s.charAt(i));
         }
         
         return stack.isEmpty();
