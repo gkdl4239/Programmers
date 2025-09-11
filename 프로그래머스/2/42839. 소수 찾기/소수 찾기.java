1de7e3c3cf8c6c13b471f1f55067fd26bbe7c7ab
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-09-12 00:45:51 +0900

    [level 2] Title: 소수 찾기, Time: 19.10 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,6 +3,18 @@ class Solution {
     
     HashSet<Integer> hash = new HashSet<>();
     
+    public int solution(String numbers) {
+        
+        int answer = 0;
+        
+        dfs(new StringBuilder(), new StringBuilder(numbers));
+        
+        for(int number : hash) {
+            if(isPrime(number)) answer++;
+        }
+        
+        return answer;        
+    }
     
     private void dfs(StringBuilder cases, StringBuilder numbers) {
         
@@ -12,12 +24,8 @@ class Solution {
         
         for(int i=0; i<numbers.length(); i++) {
             
-            StringBuilder sliced = new StringBuilder();
-            
-            sliced.append(numbers.substring(0,i));
-            sliced.append(numbers.substring(i+1));
-            
-            dfs(new StringBuilder(cases).append(numbers.charAt(i)), sliced);
+            dfs(new StringBuilder(cases).append(numbers.charAt(i)), 
+                new StringBuilder(numbers.substring(0, i)).append(numbers.substring(i+1)));
         }
     }
     
@@ -25,27 +33,12 @@ class Solution {
         
         if(number < 2) return false;
         if(number == 2) return true;
-        if (number % 2 == 0) return false;
         
-        for(int i=3; i<=Math.sqrt(number); i+=2) {
-            if(number%i==0) return false;    
+        for(int i = 2; i <= Math.sqrt(number); i++) {
+            if(number % i == 0) return false;
         }
         
         return true;
     }
     
-    
-    
-    public int solution(String numbers) {
-        
-        int answer = 0;
-        
-        dfs(new StringBuilder(), new StringBuilder(numbers));
-        
-        for(int num : hash) {
-            if(isPrime(num)) answer++;
-        }
-        
-        return answer;
-    }
 }
