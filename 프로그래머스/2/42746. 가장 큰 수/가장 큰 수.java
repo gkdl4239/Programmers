cb590e6eba5fe2bb91f22bad19c46107b6a168e9
WoojinLee <77243795+gkdl4239@users.noreply.github.com>
2025-09-11 22:54:12 +0900

    [level 2] Title: 가장 큰 수, Time: 229.69 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,20 +2,19 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         
-        StringBuilder sb = new StringBuilder();
         String[] words = new String[numbers.length];
+        StringBuilder sb = new StringBuilder();
         
         for(int i=0; i<numbers.length; i++) {
-            words[i] = Integer.toString(numbers[i]);
+            words[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(words, (o1, o2) -> (Integer.parseInt(o2+o1) - Integer.parseInt(o1+o2)));
+        Arrays.sort(words, (o1, o2) -> (Integer.parseInt(o2 + o1) - Integer.parseInt(o1 + o2)));
         
         for(String word : words) {
             sb.append(word);
         }
         
-        if(sb.charAt(0) == '0') return "0";
-        return sb.toString();
+        return sb.charAt(0) == '0' ? "0" : sb.toString();
     }
 }
